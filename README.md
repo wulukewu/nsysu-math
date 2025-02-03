081e53305ce1d29a5072a31afdc20f17065456c3
WULukeWU <68338769+WULukeWU@users.noreply.github.com>
2025-02-03 12:16:25 +0800

    feat: send new message to discord

---
+++ README.md
@@ -0,0 +1,1 @@
+# nsysu-math
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import os
+from dotenv import load_dotenv
+
 import time
 from datetime import datetime, timedelta, timezone
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+
+import discord
 
 def get_taiwan_date():
     # Get the current UTC time
@@ -18,6 +24,45 @@ def get_taiwan_date():
     formatted_time = f"{taiwan_year:03}.{now_utc_plus_8.month:02}.{now_utc_plus_8.day:02}"
 
     return formatted_time
+
+def dc_send(message, token, guild_id, channel_id):
+
+    intents = discord.Intents.default()
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'We have logged in as {client.user}')
+        guild = discord.utils.get(client.guilds, id=guild_id)
+        channel = discord.utils.get(guild.channels, id=channel_id)
+        await channel.send(message)
+        await client.close()
+
+    client.run(token)
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get environment variables with error handling
+discord_token = os.getenv('DISCORD_TOKEN')
+if not discord_token:
+    raise ValueError("DISCORD_TOKEN environment variable not set")
+
+discord_guild_id = os.getenv('DISCORD_GUILD_ID')
+if not discord_guild_id:
+    raise ValueError("DISCORD_GUILD_ID environment variable not set")
+discord_guild_id = int(discord_guild_id)
+
+discord_channel_id = os.getenv('DISCORD_CHANNEL_ID')
+if not discord_channel_id:
+    raise ValueError("DISCORD_CHANNEL_ID environment variable not set")
+discord_channel_id = int(discord_channel_id)
+
+def format_message(input_date, input_message):
+    if '\n' not in input_message:
+        return f'{input_date}: {input_message}'
+    else:
+        return f'{input_date}:\n{input_message}'
 
 # Set up ChromeDriver
 options = webdriver.ChromeOptions()
@@ -63,7 +108,11 @@ for row in reversed(rows):
         # print(f'announced_date: {announced_date}, announced_message: {announced_message}')
 
         if announced_date == now_date:
-            print('Sending message to the Discord channel')
+            print('[INFO] Sending message to the Discord channel...')
+            
+            message = format_message(announced_date, announced_message)
+            print(message)
+            dc_send(message, discord_token, discord_guild_id, discord_channel_id)
 
     else:
         print('No columns found')
